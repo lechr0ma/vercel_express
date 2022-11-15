172a149d43d141e36f2f21de4039b663487aaa0e
Alexander Khromov <alexander.khromov@slexn.com>
2022-11-15 16:44:42 +1000

    4

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const testRouter = require("./routers/testRouter");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("WOrks"));
+app.get("/", (req, res) => {
+    console.log("/ get")
+    res.send("WOrks")
+});
 
 app.listen(5000, () => console.log("app working"));
 app.use("/test", testRouter);
